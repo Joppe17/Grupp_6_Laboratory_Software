1f3a7709b7d54defeaf0248df0d760e5e9d54e71
Knut Larsson <knut.larsson@hotmail.se>
2026-04-01 11:06:09 +0200

    initial code structure (fixes #5)

---
+++ logic/game_loop.py
@@ -0,0 +1,38 @@
+import pygame
+"""
+Min tanke är att detta är vår game loop till spelet.
+Här har vi en array av objekt där alla kan lägga till egenskapade objekt.
+Alla skapade objekt kommer ha GameObjects som bas klass.
+GameObjects klassen kommer ha metoder bland annat update, draw med mera.
+
+I vår game loop kommer dessa objekt konstant kallas på och ritas på skärmen eller lyssna på knapp tryckningar osv.
+
+Vi kommer använda en teknik som kallas frame rate independence som i princip sätter samma framerate oavsett datorns hastighet.
+"""
+pygame.init()
+
+screen = pygame.display.set_mode((640,640))
+
+clock = pygame.time.Clock()
+
+objects = []
+
+running = True
+        
+while running:
+        
+    dt = clock.tick(60)/1000
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            
+            running = False
+
+    screen.fill((0,25,150))
+
+    for obj in objects:
+        obj.update(dt)
+        obj.draw(screen)
+
+    pygame.display.flip()
---
+++ logic/gamestate_class.py
@@ -0,0 +1,12 @@
+class GameState:
+"""
+Här är min tanke att vi kan spara "globala" värden som vi kan skicka in när vi skapar nya objekt.
+Detta vill jag göra så att tex vårt objekt som vi klickar på ska kunna updatera en räknare. 
+"""
+def __init__(self):
+    self.score = 0
+    self.kps = 0 #klick per sekund
+
+
+
+
