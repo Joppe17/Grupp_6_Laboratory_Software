269aea5aac230a355cd3491c0843ec49111f97fb
Oskar Ali <oskarali03@hotmail.com>
2026-04-01 14:35:23 +0200

    edited ui.py and added a class circle

--- ui/ui.py
+++ ui/ui.py
@@ -1,8 +1,11 @@
 import pygame
 
-pygame.init()
+class circle: 
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
 
-screen = pygame.display.set_mode((640, 640))
-pygame.display.set_caption("Not Cookie Clicker")
-
-pygame.quit()
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
