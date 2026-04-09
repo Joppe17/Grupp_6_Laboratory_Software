b1cb03393a90455ee44f7928299aea68fc1e023f
Knut Larsson <knut.larsson@hotmail.se>
2026-04-09 17:42:12 +0200

    Added jungle background

--- ui/background.py
+++ ui/background.py
@@ -6,7 +6,7 @@ ASSETS = os.path.join(os.path.dirname(__file__))
 class Background:
     def __init__(self, screen):
         img = pygame.image.load(os.path.join(ASSETS, "background.jpg"))
-        self.surface = pygame.transform.scale(img, (1200, 640))
+        self.surface = pygame.transform.scale(img, (1000, 700))
 
     def draw(self, screen):
         screen.blit(self.surface, self.surface.get_rect(center=screen.get_rect().center))
