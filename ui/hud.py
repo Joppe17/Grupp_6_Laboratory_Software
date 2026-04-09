7043571a069febda4c8b37416f72dc474304352e
Linus Jacobsson <jali24qy@student.ju.se>
2026-04-09 17:41:29 +0200

    Fixed the font, made it jungle-themed

--- ui/hud.py
+++ ui/hud.py
@@ -1,14 +1,15 @@
 import pygame
+import os
 
 class Hud:
     def __init__(self, screen):
-        self.font = pygame.font.SysFont('comicsansms', 60)
-        self.text_color = (255, 165, 0)
+        self.font = pygame.font.Font(os.path.join(os.path.dirname(__file__), "Jungle.otf"), 60)
+        self.text_color = (1, 50, 32)
         self.shadow_color = (0, 0, 0)
 
     def draw(self, screen, counter):
-        text = self.font.render(f'Clicks : {counter}', True, self.text_color)
-        shadow = self.font.render(f'Clicks : {counter}', True, self.shadow_color)
-        rect = text.get_rect(center=(320, 50))
+        text = self.font.render(f'CLICKS : {counter}', True, self.text_color)
+        shadow = self.font.render(f'CLICKS : {counter}', True, self.shadow_color)
+        rect = text.get_rect(center=(320, 70))
         screen.blit(shadow, (rect.x + 3, rect.y + 3))
         screen.blit(text, rect)
