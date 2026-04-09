3af0a0fb5b10a55b69c968aaea9f0d95fa2523dd
Linus Jacobsson <jali24qy@student.ju.se>
2026-04-09 16:05:34 +0200

    Added increment logic to circle

--- ui/circle.py
+++ ui/circle.py
@@ -1,11 +1,21 @@
 import pygame
+import math
+from logic.game_object import GameObject 
 
-class Circle:
-    def __init__(self, x, y, radius, color):
+class Circle(GameObject):
+    def __init__(self, x, y, radius, color, state):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
+        self.state = state
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            dx = event.pos[0] - self.x
+            dy = event.pos[1] - self.y
+            if math.sqrt(dx * dx + dy * dy) <= self.radius:
+                self.state.score += 1
 
     def update(self, dt):
         pass
