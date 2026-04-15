05c6582bac45d8b72b67493257c1b6335eacc3f1
Elias Sjögren <sjel24no@student.ju.se>
2026-04-15 10:37:07 +0200

    added unittest to test the functionality of the flyingbanana ond the multiplication feature

---
+++ unittest/bananatest.py
@@ -0,0 +1,64 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+import pytest
+import pygame
+from ui.banana import flyingBanana
+from logic.game_state import GameState
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+class TestGameState:
+    def test__initial_score(self):
+        state = GameState()
+        assert state.score == 0
+    
+    def test_initial_multiplier(self):
+        state = GameState()
+        assert state.click_multiplier == 1
+
+    def test_bonus_activation(self):
+        state = GameState()
+        state.click_multiplier = 3
+        state.score += 1 * state.click_multiplier
+        assert state.score == 3
+
+class TestBanana:
+    def test_starts_inactive(self):
+        state = GameState()
+        banana = flyingBanana(screen, state)
+        assert banana.active == False
+
+    def test_spawn(self):
+        state = GameState()
+        banana = flyingBanana(screen, state)
+        banana.spawn()
+        assert banana.active == True
+        assert 60 <= banana.x <= 740
+        assert banana.y == -80
+
+    def test_bonus_activation_on_click(self):
+        state = GameState()
+        banana = flyingBanana(screen, state)
+        banana.spawn()
+        click_event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, pos=(banana.x + 10, banana.y + 10))
+        banana.handle_event(click_event)
+        assert state.click_multiplier == 3
+        assert banana.bonus_active == True
+    
+    def test_bonus_expires(self):
+        state = GameState()
+        banana = flyingBanana(screen, state)
+        banana.spawn()
+        click_event = pygame.event.Event(pygame.MOUSEBUTTONDOWN, pos=(banana.x + 10, banana.y + 10))
+        banana.handle_event(click_event)
+        assert state.click_multiplier == 3
+        assert banana.bonus_active == True
+
+        # Simulerar tid för att bonusen ska gå ut
+        dt = flyingBanana.duration + 1
+        banana.update(dt)
+        assert state.click_multiplier == 1
+        assert banana.bonus_active == False
